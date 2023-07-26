e6055618c89ecee4672570a0fc58af1d3132c032
1337CodeGod <51419091+1337CodeGod@users.noreply.github.com>
2023-07-26 16:14:32 -0400

    v1

---
+++ Older Version/Program copy.cs
@@ -0,0 +1,24 @@
+﻿// // This program sends a windows notification to the user every x minutes.
+// // The the user can specify the time interval and the message to be displayed.
+
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
+// using System.Windows.Forms;
+// using System.Threading;
+
+// class Program {
+
+//     static void Main(string[] args) {
+//         Console.WriteLine("Enter the time interval in minutes: ");
+//         int timeInterval = 30;
+//         Console.WriteLine("Enter the message to be displayed: ");
+//         string message = "Drink water.";
+//         while (true) {
+//             Thread.Sleep(timeInterval * 60 * 1000);
+//             MessageBox.Show(message, "", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+//         }
+//     }
+// }
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// This program sends a windows notification to the user every x minutes.
+// The the user can specify the time interval and the message to be displayed.
+class Program {
+
+    static void Main(string[] args) {
+        int timeInterval = 30;
+        string message = "Drink water.";
+        while (true) {
+            MessageBox.Show(message, "", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);            
+            Thread.Sleep(timeInterval * 60 * 1000);
+        }
+    }
+}
